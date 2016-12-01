ea61f1668f080229ae60c190e36c8c50ed6910cd
Tom Chapin <tchapin@gmail.com>
2016-11-30 23:49:06 -0700

    fix expect syntax

--- spec/features/specs/remote_start_spec.rb
+++ spec/features/specs/remote_start_spec.rb
@@ -5,15 +5,14 @@ require 'spec_helper'
 feature 'Hyundai BlueLink - Remote Start', type: :feature, js: true do
 
   scenario 'logging into Hyundai BlueLink and remote starting the car' do
+    puts 'Loading the My Hyundai site'
     home_page.load
 
+    puts 'Logging in'
     home_page.login(email_address: ENV['HYUNDAI_USERNAME'], password: ENV['HYUNDAI_PASSWORD'])
 
-    # Wait for the log in process to complete and for the VIN to show on the dashboard
-    page.should have_content(ENV['HYUNDAI_VIN'])
-
-    binding.pry
-
+    puts 'Waiting for the log in process to complete and for the VIN to show on the dashboard'
+    expect(page).to have_content(ENV['HYUNDAI_VIN'])
   end
 
 end
